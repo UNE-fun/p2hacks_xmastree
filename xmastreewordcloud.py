b270bd4ca55f9a0188f3542b25566e807806cdc7
une <datbmp338@hotmail.co.jp>
2019-12-13 21:50:52 +0900

    ADD: 日本語フォントを追加・パスを指定。wordcloudが豆腐以外を出力するようになった

--- xmastreewordcloud.py
+++ xmastreewordcloud.py
@@ -4,24 +4,23 @@
 from PIL import Image
 import numpy as np
 from MeCab import Tagger
-from wordcloud import WordCloud
+import wordcloud
+import datetime
 
 # wordcloudを作成する関数
 """
 TODO: 文字列から成るtweetsを元に、wordcloudの画像を生成し、`/static/images/`内に保存、その画像のパス(ファイル名だけで良い)を返す。
 画像のファイル名は、`2019-12-13T12:00:00`とし、これは日本時間によるものとする。
-引数：tweets 型：string
+TODO: stringが引数だと想定して開発をしていた。arrで動作するように修正をする。
+引数：tweets 型：arr
 出力：YYYY-MM-DDTHH:MM:SS.png
 """
 def gen_wordcloud(tweets):
-    t = Tagger()
     # 日本語をスペース区切りのテキストにし、splittextに格納する
-    splitedtext =text_split(tweets)
-    # 一文字以上の語全てを抽出する
-    wc = WordCloud(regexp="[\w']+")
+    splitedtext = text_split(tweets)
 
     # maskを作成する
-    mask = make_maskarr(maskimage_path)
+    #mask = make_maskarr(maskimage_path)
 
     # wordcloudを作成する
     # WordCloud中の引数について
@@ -31,15 +30,15 @@ def gen_wordcloud(tweets):
         # countour_color: 境界線の色
         # width: 出力画像の幅
         # height: 出力画像の高さ
-    wc.wordcloud.WordCloud(background_color='white', mask=mask, countour_width=3, contour_color='green', width=800, height=600).generate(splitedtext)
-
-    # matplotlibを用いて表示を行う
-    # plt.imshow(wc)
-    # plt.show()
+    # wc = wordcloud.WordCloud(regexp="[\w']+", background_color='white', mask=mask, countour_width=3, contour_color='green', width=800, height=600).generate(splitedtext)
+    wc = wordcloud.WordCloud(font_path="fonts/NotoSansCJKjp-Regular.otf", regexp="[\w']+", background_color='white', width=800, height=600).generate(splitedtext)
 
     # 画像として保存を行う
-    wcimage_path = save_wcimage(wc, nowtime)
-    return wcimage_path
+    # nowtime = datetime.datetime.now()
+    # wcimage_path = save_wcimage(wc, nowtime)
+    # return wcimage_path
+    wc.to_file("test.png")
+    return 0
 
 # 文字列をスペース区切りのテキストにし、助詞助動詞を取り除く関数
 """
@@ -47,7 +46,8 @@ def gen_wordcloud(tweets):
 出力： 型：
 """
 def text_split(texts):
-    return  " ".join([x.split("\t")[0] for x in t.parse(texts).splitlines()[:-1] not in ["助詞", "助動詞"]])
+    t = Tagger()
+    return  " ".join([x.split("\t")[0] for x in t.parse(texts).splitlines()[:-1] if x.split("\t")[1].split(",")[0] not in ["助詞", "助動詞"]])
 
 # mask画像の配列を作成する関数
 """
@@ -62,10 +62,11 @@ def make_maskarr(maskimage_path):
 # wc画像を保存、保存先のパスを返す関数
 """
 # TODO: 日付を取得してくる
-引数：wc
+引数1：wc
+引数2：nowtime 型：datetimeオブジェクト
 出力：wcimage_path 型：string
 """
 def save_wcimage(wc, nowtime):
-    wcimage_path = nowtime + "png"
-    wc.to_file("/static/images/" + wcimage_path)
+    wcimage_path = nowtime.strftime("%Y-%M-%DT%H:%M:00") + "png"
+    wc.to_file("./static/images/" + wcimage_path)
     return wcimage_path
